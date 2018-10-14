1797e5b3b6823c0d13439893135a2d3146a1d2c1
Edson César <edsono@gmail.com>
2018-10-14 11:14:27 -0400

    Melhorias na configuração do fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,10 @@ set -gx EDITOR vim
 
 ### Home/bin
 set --global fish_user_paths $fish_user_paths ~/bin
+
+if not functions -q fisher
+    echo "Installing fisher for the first time..." >&2
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fisher
+end
