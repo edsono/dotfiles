fcb836636aa6e477793c0f55d1de3b51897e26b1
Edson César <edsono@gmail.com>
2024-05-17 11:39:01 -0400

    Ignorar lazy-lock.json

--- bin/executable_install-brew-linux.sh
+++ bin/executable_install-brew-linux.sh
@@ -3,3 +3,6 @@
 set -e
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install modern Unix
+brew install fzf bat git-delta eza fd tldr cheat dust duf broot ripgrep zoxide
