44f9e22aa4458e84c4c6e783e1623e463108340c
Edson César <edsono@gmail.com>
2024-05-17 11:45:24 -0400

    Corrige instalação do Homebrew

--- bin/executable_install-brew-linux.sh
+++ bin/executable_install-brew-linux.sh
@@ -2,7 +2,9 @@
 # Install Homebrew (https://brew.sh/) for Linux
 set -e
 
+sudo yum groupinstall 'Development Tools'
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install modern Unix
-brew install fzf bat git-delta eza fd tldr cheat dust duf broot ripgrep zoxide
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+brew install gcc fzf bat git-delta eza fd tldr cheat dust duf broot ripgrep zoxide
