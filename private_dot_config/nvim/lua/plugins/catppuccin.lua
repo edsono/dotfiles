700d03719f246c2f367d7e8c330d0535f6f3ecbf
Edson César <edsono@gmail.com>
2024-05-23 14:24:39 -0400

    Atualizando catppuccin

--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -30,24 +30,36 @@ return {
         },
         custom_highlights = function(cp)
           return {
-            -- Gray like: Overlay 0, Overlay 1, Overlay 2
+            -- Gray like: Text, Overlay 0, Overlay 1, Overlay 2
             Comment = { fg = cp.overlay0 }, -- just comments
             SpecialComment = { link = "Special" }, -- special things inside a comment
             Identifier = { fg = cp.text }, -- (preferred) any variable name
+            ["@field"] = { fg = cp.text },
+            ["@variable"] = { fg = cp.text },
+            ["@property"] = { fg = cp.text },
+            ["@parameter"] = { fg = cp.text, style = {} },
+            ["@variable.builtin"] = { fg = cp.text, style = {} },
+            ["@keyword.luadoc"] = { link = "Comment" },
 
             -- Purple like: Mauve, Lavender
-            Constant = { fg = cp.mauve }, -- (preferred) any constant
             Boolean = { fg = cp.mauve }, --  a boolean constant: TRUE, false
+            Constant = { fg = cp.mauve }, -- (preferred) any constant
+            ["@constant.regex"] = { fg = cp.teal },
+            ["@constant.builtin"] = { fg = cp.flamingo, style = {} },
+
 
             -- Green like: Green, Teal
-            String = { fg = cp.green }, -- a string constant: "this is a string"
             Character = { fg = cp.teal }, --  a character constant: 'c', '\n'
+            String = { fg = cp.green }, -- a string constant: "this is a string"
+            ["@string.escape"] = { link = "Character" },
+            ["@operator.regex"] = { link = "Character" },
+            ["@punctuation.bracket.regex"] = { link = "Character" },
 
             -- Blue like: Blue, Sky, Sapphire
             Number = { fg = cp.sky }, --   a number constant: 234, 0xff
             Float = { fg = cp.sky }, --    a floating point constant: 2.3e10
 
-            -- Orange like: Yellow, Peach, Rosewater
+            -- Orange like: Peach, Flamingo
             Function = { fg = cp.text }, -- function name (also: methods for classes)
             Statement = { fg = cp.peach }, -- (preferred) any statement
             Conditional = { fg = cp.peach, style = {} }, --  if, then, else, endif, switch, etc.
@@ -56,6 +68,11 @@ return {
             Exception = { fg = cp.peach }, --  try, catch, throw
             Keyword = { fg = cp.peach }, --  any other keyword
             Operator = { fg = cp.rosewater }, -- "sizeof", "+", "*", etc.
+            ["@namespace"] = { fg = cp.flamingo, style = {} },
+            ["@constructor"] = { fg = cp.flamingo },
+            ["@keyword.return"] = { fg = cp.peach },
+            ["@keyword.function"] = { fg = cp.peach },
+            ["@keyword.operator"] = { fg = cp.peach },
 
             -- Red like: Red, Marron, Pink
             PreProc = { fg = cp.red }, -- (preferred) generic Preprocessor
@@ -64,41 +81,25 @@ return {
             Macro = { fg = cp.red }, -- same as Define
             PreCondit = { link = "PreProc" }, -- preprocessor #if, #else, #endif, etc.
 
-            -- Yellow like: Yellow, Flamingo
-            StorageClass = { fg = cp.flamingo }, -- static, register, volatile, etc.
+            -- Yellow like: Yellow, Rosewater
+            StorageClass = { fg = cp.rosewater }, -- static, register, volatile, etc.
             Structure = { fg = cp.yellow }, --  struct, union, enum, etc.
             Special = { fg = cp.yellow }, -- (preferred) any special symbol
-            Type = { fg = cp.flamingo }, -- (preferred) int, long, char, etc.
+            Type = { fg = cp.rosewater }, -- (preferred) int, long, char, etc.
             Typedef = { link = "Type" }, --  A typedef
+            ["@type.builtin"] = { link = "Type" },
 
             SpecialChar = { link = "Special" }, -- special character in a constant
             Tag = { fg = cp.lavender, style = { "bold" } }, -- you can use CTRL-] on this
             Delimiter = { fg = cp.overlay2 }, -- character that needs attention
             Debug = { link = "Special" }, -- debugging statements
 
+            -- Todo, fixme
             TodoFgTODO = { fg = cp.mauve },
             TodoBgTODO = { bg = cp.mauve },
 
-            ["@field"] = { fg = cp.text },
-            ["@variable"] = { fg = cp.text },
-            ["@property"] = { fg = cp.text },
-            ["@parameter"] = { fg = cp.rosewater, style = {} },
-            ["@namespace"] = { fg = cp.rosewater, style = {} },
-
-            ["@constructor"] = { fg = cp.flamingo },
-            ["@type.builtin"] = { fg = cp.flamingo },
-            ["@variable.builtin"] = { fg = cp.flamingo, style = {} },
-            ["@constant.builtin"] = { fg = cp.flamingo, style = {} },
-
-            ["@string.escape"] = { fg = cp.teal },
-            ["@constant.regex"] = { fg = cp.teal },
-            ["@operator.regex"] = { fg = cp.teal },
-            ["@punctuation.bracket.regex"] = { fg = cp.teal },
-            ["@string.documentation.python"] = { fg = cp.overlay0, style = { "italic" } },
-
-            ["@keyword.return"] = { fg = cp.peach },
-            ["@keyword.function"] = { fg = cp.peach },
-            ["@keyword.operator"] = { fg = cp.peach },
+            -- Pyhton
+            ["@string.documentation.python"] = { link = "Comment", style = { "italic" } },
           }
         end,
       })
