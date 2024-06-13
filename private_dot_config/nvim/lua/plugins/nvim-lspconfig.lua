074bc08bef6b149e80efd1dea8c365cf76e872b4
Edson César <edsono@gmail.com>
2024-06-13 09:09:14 -0400

    fix: corrige problema com duplicação de linting entre ruff_lsp e pyright

--- private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,73 +1,47 @@
 return {
   "neovim/nvim-lspconfig",
+  -- https://github.com/LazyVim/LazyVim/issues/1819#issuecomment-1953895530
   opts = {
     servers = {
-      gopls = {
-        keys = {
-          -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
-          { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
-        },
+      pyright = {
         settings = {
-          gopls = {
-            gofumpt = true,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
+          pyright = {
+            disableOrganizeImports = true, -- Using Ruff
+          },
+          python = {
+            analysis = {
+              ignore = { "*" }, -- Using Ruff
+              -- typeCheckingMode = "off", -- Using mypy
             },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            analyses = {
-              fieldalignment = false,
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-            semanticTokens = true,
+          },
+        },
+      },
+      ruff_lsp = {
+        keys = {
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
           },
         },
       },
     },
     setup = {
-      ---@diagnostic disable-next-line: unused-local
-      gopls = function(_, opts)
-        -- workaround for gopls not supporting semanticTokensProvider
-        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+      ruff_lsp = function()
         require("lazyvim.util").lsp.on_attach(function(client, _)
-          if client.name == "gopls" then
-            if not client.server_capabilities.semanticTokensProvider then
-              local semantic = client.config.capabilities.textDocument.semanticTokens
-              client.server_capabilities.semanticTokensProvider = {
-                full = true,
-                legend = {
-                  ---@diagnostic disable-next-line: need-check-nil
-                  tokenTypes = semantic.tokenTypes,
-                  ---@diagnostic disable-next-line: need-check-nil
-                  tokenModifiers = semantic.tokenModifiers,
-                },
-                range = true,
-              }
-            end
+          if client.name == "ruff_lsp" then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
           end
         end)
-        -- end workaround
       end,
     },
   },
